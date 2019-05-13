189b1d71c1f84f4f46b736ed6ba746cb1ead07e5
daniel <daniel@UBUNTU18>
2019-05-13 16:13:30 +0200

    Añadido generateName a generateAnimals

--- src/utils/GenerateAnimals.java
+++ src/utils/GenerateAnimals.java
@@ -5,15 +5,22 @@
  */
 package utils;
 
+import java.util.Random;
+
 /**
  *
  * @author daniel
  */
 public class GenerateAnimals {
     
+    public static Random random = new Random();
+    
     public static String generateName() {
-        String name = "BArtodlo";
-        return name;
+        String[] array = "{Abba,Ada,Alaska,Atila,Apolo,Ary,Bart,Bali,Bella,Bigotes,Blondie,Bola,Bran,Brown,Bruno,Brenda,Bruce,Bu,Candy,Chester,Chispa,Coco,Dina,Duna,Emily,Elsa,Emy,Fer,Flash,Flopy,Fox,Gandhi,Hachiko,Iron,Iris,Jerry,Kim,Kiwi,Lázaro,Lily,Morey,Murphy,Neo,Nico,Novo,Orion,Osa,Panda,Perla,Pipa,Rayo,Roco,Ron,Sam,Snow,Ucla,Ursus,Urko ,West ,Yoko}".split(",");
+        int choice = random.nextInt(array.length);
+        return array[choice];
     }
+    
+    
     
 }
---
+++ src/utils/GeneratePeople.java
@@ -0,0 +1,70 @@
+package utils;
+
+
+
+
+import java.util.Random;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daniel
+ */
+public class GeneratePeople {
+    
+    public static Random random = new Random();
+    
+    public static String generateName(){
+        String[] array = "Viki,Toya,Arlene,Gregory,Vennie,Sharla,Sharie,Kiyoko,Vilma,Tempie,Donny,Shaunda,Illa,Dewey,Ping,Evette,Leslie,Kimberley,Gwen,Sharon,Lawerence,Charlotte,Bennie,Kathey,Aaron,Madalene,Hye,Rosamond,Filomena,Lourie,Shery,Luciano,Rolf,Evita,Everett,Taunya,Elton,Elfrieda,Zulma,Tarra,Corrina,Johnie,Tifany,Tiana,Sandi,Chloe,Lavada,Kali,Gemma,Ariana".split(",");
+        int choice = random.nextInt(array.length);
+        return array[choice];
+    }
+    
+    public static String generateAddress(){
+        String[] array = "Mordor,Granollers,Mercadona,Neverland,Barcelona,Madrid,Edinburgo,Paris,Berlín,Mongolia,Carrefour".split(",");
+        int choice = random.nextInt(array.length);
+        return array[choice];
+    }
+    
+    public static int generatePhoneNumber(){
+        String number = "";
+        for (int i = 0; i < 9; i++) {
+            number += String.valueOf(random.nextInt(9));
+        }
+        
+        return Integer.parseInt(number);
+    }
+    
+    private static String getDNILetter(int DNI){
+        String[] letters = "T,R,W,A,G,M,Y,F,P,D,X,B,N,J,Z,S,Q,V,H,L,C,K,E".split(",");
+        
+        return letters[DNI%23];
+        
+    }
+    
+    public static String generateDNI(){        
+        
+        String DNI = "";
+        for (int i = 0; i < 8; i++) {
+            DNI += String.valueOf(random.nextInt(9));
+        }
+        
+        DNI += getDNILetter(Integer.valueOf(DNI));
+        
+        return DNI; 
+    }
+    
+    public static double generateGrade(){
+        return random.nextDouble()*11;
+    }
+    
+    public static int generateAge(){
+        return random.nextInt(99)+1;
+    }
+    
+}
