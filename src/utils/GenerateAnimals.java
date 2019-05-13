c913d46fceff1b786be095095948f8a17fc09b51
daniel <daniel@UBUNTU18>
2019-05-13 16:23:48 +0200

    Borradas las {} innecesarias

--- src/utils/GenerateAnimals.java
+++ src/utils/GenerateAnimals.java
@@ -16,13 +16,13 @@ public class GenerateAnimals {
     public static Random random = new Random();
     
     public static String generateName() {
-        String[] array = "{Abba,Ada,Alaska,Atila,Apolo,Ary,Bart,Bali,Bella,Bigotes,Blondie,Bola,Bran,Brown,Bruno,Brenda,Bruce,Bu,Candy,Chester,Chispa,Coco,Dina,Duna,Emily,Elsa,Emy,Fer,Flash,Flopy,Fox,Gandhi,Hachiko,Iron,Iris,Jerry,Kim,Kiwi,Lázaro,Lily,Morey,Murphy,Neo,Nico,Novo,Orion,Osa,Panda,Perla,Pipa,Rayo,Roco,Ron,Sam,Snow,Ucla,Ursus,Urko ,West ,Yoko}".split(",");
+        String[] array = "Abba,Ada,Alaska,Atila,Apolo,Ary,Bart,Bali,Bella,Bigotes,Blondie,Bola,Bran,Brown,Bruno,Brenda,Bruce,Bu,Candy,Chester,Chispa,Coco,Dina,Duna,Emily,Elsa,Emy,Fer,Flash,Flopy,Fox,Gandhi,Hachiko,Iron,Iris,Jerry,Kim,Kiwi,Lázaro,Lily,Morey,Murphy,Neo,Nico,Novo,Orion,Osa,Panda,Perla,Pipa,Rayo,Roco,Ron,Sam,Snow,Ucla,Ursus,Urko ,West ,Yoko".split(",");
         int choice = random.nextInt(array.length);
         return array[choice];
     }
     
     public static String generateSpecies(){
-        String [] array = "{Gato, Tortuga, León, Perro, Serpiente, Loro, Elefante, Lagartija, Mono, Perezoso, Pez}".split(",");
+        String [] array = "Gato, Tortuga, León, Perro, Serpiente, Loro, Elefante, Lagartija, Mono, Perezoso, Pez".split(",");
         int choice = random.nextInt(array.length);
         return array[choice];
     }
