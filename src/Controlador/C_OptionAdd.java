76b1d239fb86dc59a3dd92e653ab03034262d301
noelia <noelia@noelia>
2019-05-16 19:00:22 +0200

    Creado controlador de opciones de agregado

---
+++ src/Controlador/C_OptionAdd.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Vista.V_AddAnimal;
+import Vista.V_AddCuidador;
+import Vista.V_AddVisita;
+import Vista.V_OptionAdd;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author noelia
+ */
+public class C_OptionAdd {
+
+    V_OptionAdd v_optionAdd;
+
+    public C_OptionAdd(V_OptionAdd v_optionAdd) {
+        this.v_optionAdd = v_optionAdd;
+
+        // Al pulsar Animal
+        this.v_optionAdd.bttn_addAnimal.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                V_AddAnimal v_addAnimal = new V_AddAnimal();
+                v_addAnimal.setVisible(true);
+            }
+        });
+
+        // Al pulsar Cuidador
+        this.v_optionAdd.bttn_addCuidador.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                V_AddCuidador v_addCuidador = new V_AddCuidador();
+                v_addCuidador.setVisible(true);
+            }
+        });
+
+        // Al pulsar Visita
+        this.v_optionAdd.bttn_addVisita.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                V_AddVisita v_addVisita = new V_AddVisita();
+                v_addVisita.setVisible(true);
+            }
+        });
+
+    }
+
+}
