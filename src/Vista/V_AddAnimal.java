ef35282e133d60dd5eccb91729a8a82d6bc90ac1
daniel <daniel@UBUNTU18>
2019-05-16 19:15:14 +0200

    Cambiado el texto del botón

--- src/Vista/V_AddAnimal.java
+++ src/Vista/V_AddAnimal.java
@@ -54,7 +54,7 @@ public class V_AddAnimal extends javax.swing.JFrame {
         });
         getContentPane().add(input_nombreAnimal, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 130, 124, -1));
 
-        bttn_addAnimal.setText("Enviar");
+        bttn_addAnimal.setText("Añadir");
         getContentPane().add(bttn_addAnimal, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 260, 80, 40));
 
         jLabel1.setForeground(new java.awt.Color(1, 1, 1));
