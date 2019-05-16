16ca033cd53fb9663378552547ee4078d1e165a9
daniel <daniel@UBUNTU18>
2019-05-16 12:32:46 +0200

    Movido todo el código para generar datos de prueba a BDUtil

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -37,43 +37,7 @@ public class Main {
 //            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 //        }
         
-        AddAnimalDAO addAnimalDAO = new AddAnimalDAO();
-        AddCuidadorDAO addCuidadorDAO = new AddCuidadorDAO();
-        
-        // Crear animales
-        M_Animal a1 = new M_Animal(GenerateAnimals.generateName(), GenerateAnimals.generateSpecies(), "");
-        M_Animal a2 = new M_Animal(GenerateAnimals.generateName(), GenerateAnimals.generateSpecies(), "");
-        M_Animal a3 = new M_Animal(GenerateAnimals.generateName(), GenerateAnimals.generateSpecies(), "");
-        M_Animal a4 = new M_Animal(GenerateAnimals.generateName(), GenerateAnimals.generateSpecies(), "");
-        M_Animal a5 = new M_Animal(GenerateAnimals.generateName(), GenerateAnimals.generateSpecies(), "");
-        M_Animal a6 = new M_Animal(GenerateAnimals.generateName(), GenerateAnimals.generateSpecies(), "");
-        
-        // Crear cuidadores
-        M_Cuidador c1 = new M_Cuidador(GeneratePeople.generateDNI(), GeneratePeople.generateName(), GeneratePeople.generateAddress(), String.valueOf(GeneratePeople.generatePhoneNumber()));
-        M_Cuidador c2 = new M_Cuidador(GeneratePeople.generateDNI(), GeneratePeople.generateName(), GeneratePeople.generateAddress(), String.valueOf(GeneratePeople.generatePhoneNumber()));
-        M_Cuidador c3 = new M_Cuidador(GeneratePeople.generateDNI(), GeneratePeople.generateName(), GeneratePeople.generateAddress(), String.valueOf(GeneratePeople.generatePhoneNumber()));
-        M_Cuidador c4 = new M_Cuidador(GeneratePeople.generateDNI(), GeneratePeople.generateName(), GeneratePeople.generateAddress(), String.valueOf(GeneratePeople.generatePhoneNumber()));
-        M_Cuidador c5 = new M_Cuidador(GeneratePeople.generateDNI(), GeneratePeople.generateName(), GeneratePeople.generateAddress(), String.valueOf(GeneratePeople.generatePhoneNumber()));
-        
-        
-        try {
-            // Añadir animales
-            addAnimalDAO.addAnimal(a1);
-            addAnimalDAO.addAnimal(a2);
-            addAnimalDAO.addAnimal(a3);
-            addAnimalDAO.addAnimal(a4);
-            addAnimalDAO.addAnimal(a5);
-            addAnimalDAO.addAnimal(a6);
-            
-            // Añadir cuidadores
-            addCuidadorDAO.addCuidador(c1);
-            addCuidadorDAO.addCuidador(c2);
-            addCuidadorDAO.addCuidador(c3);
-            addCuidadorDAO.addCuidador(c4);
-            addCuidadorDAO.addCuidador(c5);
-        } catch (SQLException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        BDUtil.insertarDatos();
         
         
     }
