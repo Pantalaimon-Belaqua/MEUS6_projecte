d2cfee74ebff71ead061f81c379f2d6e4bbeb498
noelia <noelia@noelia>
2019-05-17 15:17:12 +0200

    Creando el contenido del controlador del cuidador

--- src/Controlador/C_AddCuidador.java
+++ src/Controlador/C_AddCuidador.java
@@ -5,10 +5,39 @@
  */
 package Controlador;
 
+import Modelo.M_Cuidador;
+import Pers.AddCuidadorDAO;
+import Vista.V_AddCuidador;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 /**
  *
  * @author noelia
  */
 public class C_AddCuidador {
+    
+    public V_AddCuidador v_addCuidador;
+    private AddCuidadorDAO cuidadorDAO = new AddCuidadorDAO();
+    M_Cuidador m_cuidador;
+
+    public C_AddCuidador(V_AddCuidador v_addCuidador) {
+        this.v_addCuidador = v_addCuidador;
+        
+       this.v_addCuidador.bttn_enviar.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            
+            
+            }
+        });
+    }
+    
+    
+    
     
 }
