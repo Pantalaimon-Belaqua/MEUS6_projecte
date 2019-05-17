ff538759c94b42265baf19776c77ec62b1eb8c7c
daniel <daniel@UBUNTU18>
2019-05-17 16:30:21 +0200

    Ahora la ventana se cierra al abrir otra

--- src/Controlador/C_OptionAdd.java
+++ src/Controlador/C_OptionAdd.java
@@ -11,6 +11,7 @@ import Vista.V_AddVisita;
 import Vista.V_OptionAdd;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 /**
  *
@@ -30,6 +31,7 @@ public class C_OptionAdd {
                 V_AddAnimal v_addAnimal = new V_AddAnimal();
                 C_AddAnimal c_addAnimal = new C_AddAnimal(v_addAnimal);
                 v_addAnimal.setVisible(true);
+                v_optionAdd.dispatchEvent(new WindowEvent(v_optionAdd, WindowEvent.WINDOW_CLOSING));
             }
         });
 
@@ -41,6 +43,7 @@ public class C_OptionAdd {
                 V_AddCuidador v_addCuidador = new V_AddCuidador();
                 C_AddCuidador c_addCuidador = new C_AddCuidador(v_addCuidador);
                 v_addCuidador.setVisible(true);
+                v_optionAdd.dispatchEvent(new WindowEvent(v_optionAdd, WindowEvent.WINDOW_CLOSING));
             }
         });
 
@@ -51,8 +54,12 @@ public class C_OptionAdd {
             public void actionPerformed(ActionEvent e) {
                 V_AddVisita v_addVisita = new V_AddVisita();
                 v_addVisita.setVisible(true);
+                v_optionAdd.dispatchEvent(new WindowEvent(v_optionAdd, WindowEvent.WINDOW_CLOSING));
             }
         });
+        
+        // Poner la ventana al medio
+        v_optionAdd.setLocationRelativeTo(null);
 
     }
 
