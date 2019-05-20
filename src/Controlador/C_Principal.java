f2aca6dcf863497220c895ae9ad1a9bc5b1d117d
daniel <daniel@UBUNTU18>
2019-05-20 16:14:05 +0200

    Ahora se comprueba que haya algo seleccionado antes de intenter ver las visitas

--- src/Controlador/C_Principal.java
+++ src/Controlador/C_Principal.java
@@ -37,35 +37,35 @@ import javax.swing.table.TableRowSorter;
  * @author daniel
  */
 public class C_Principal {
-    
+
     public V_Principal v_principal;
     private PrincipalDAO principalDAO = new PrincipalDAO();
 
     public C_Principal(V_Principal v_principal) {
         this.v_principal = v_principal;
-        
+
         // Listeners para la ventana
         this.v_principal.addWindowListener(new WindowAdapter() {
 
             // Cuando se abre la ventana
             @Override
             public void windowActivated(WindowEvent e) {
-                
+
                 // Oculta la nota del buscador
                 v_principal.notaBuscador.setVisible(false);
-                
+
                 // Cambiar el tamaño de la primera columna
                 v_principal.tablaAnimales.getColumnModel().getColumn(0).setPreferredWidth(5);
-                
+
                 // Poner todos los datos tipo String centrados
                 DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
                 centerRenderer.setHorizontalAlignment(JLabel.CENTER);
                 v_principal.tablaAnimales.setDefaultRenderer(String.class, centerRenderer);
-                
+
                 updateTable();
-                
+
             }
-            
+
             // Cuando se cierra la ventana, desconectar de la BBDD
             @Override
             public void windowClosing(WindowEvent e) {
@@ -76,23 +76,21 @@ public class C_Principal {
                     Logger.getLogger(C_Principal.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            
-            
-            
+
         });
-        
+
         // Buscar
         this.v_principal.buscador.addKeyListener(new KeyAdapter() {
 
             @Override
             public void keyReleased(KeyEvent e) {
-                
+
                 String texto = v_principal.buscador.getText();
-                
-                if(!texto.isEmpty()){
+
+                if (!texto.isEmpty()) {
                     // Coger el modelo de la tabla
                     TableModel model = v_principal.tablaAnimales.getModel();
-                    
+
                     // Filtar por la palabra buscada
                     TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(model);
                     sorter.setRowFilter(RowFilter.regexFilter(texto));
@@ -100,16 +98,16 @@ public class C_Principal {
                 } else {
                     v_principal.tablaAnimales.setRowSorter(null);
                 }
-                
+
             }
-            
+
         });
-        
+
         // Nota del buscador
         this.v_principal.buscador.addFocusListener(new FocusAdapter() {
 
-            JLabel buscador = v_principal.notaBuscador;            
-            
+            JLabel buscador = v_principal.notaBuscador;
+
             @Override
             public void focusGained(FocusEvent e) {
                 buscador.setVisible(true);
@@ -119,43 +117,41 @@ public class C_Principal {
             public void focusLost(FocusEvent e) {
                 buscador.setVisible(false);
             }
-            
-        
+
         });
-        
+
         // Al pulsar añadir
         this.v_principal.botonAñadir.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                
-                if(v_principal.tablaAnimales.getSelectedRow() == -1){
-                    
+
+                if (v_principal.tablaAnimales.getSelectedRow() == -1) {
+
                     JOptionPane.showMessageDialog(v_principal, "Por favor, selecciona un animal primero", "", JOptionPane.WARNING_MESSAGE);
-                    
+
                 } else {
                     V_OptionAdd v_optionAdd = new V_OptionAdd();
                     C_OptionAdd c_optionAdd = new C_OptionAdd(v_optionAdd, getSelectedAnimalID());
                     v_optionAdd.setVisible(true);
                 }
-                
-                
+
             }
         });
-        
+
         // Al pulsar eliminar, coger el id del animal seleccionado y borrarlo
         this.v_principal.botonEliminar.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                
-                if(v_principal.tablaAnimales.getSelectedRow() == -1){
-                    
+
+                if (v_principal.tablaAnimales.getSelectedRow() == -1) {
+
                     JOptionPane.showMessageDialog(v_principal, "Por favor, selecciona un animal primero", "", JOptionPane.WARNING_MESSAGE);
-                    
+
                 } else {
                     int id = getSelectedAnimalID();
-                
+
                     try {
                         principalDAO.deleteAnimal(id);
                     } catch (SQLException ex) {
@@ -165,54 +161,57 @@ public class C_Principal {
                     // Refrescar la tabla
                     updateTable();
                 }
-                
-                
+
             }
         });
-        
+
         // Al pulsar "ver visita"
         this.v_principal.bttn_verVisita.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Coger el id del animal
-                int id = getSelectedAnimalID();
-                
-                try {
-                    if(principalDAO.hasVisits(id)){
-                        V_VerVisita v_verVisita = new V_VerVisita();
-                        C_VerVisita c_verVisita = new C_VerVisita(v_verVisita, id);
-                        v_verVisita.setVisible(true);
-                    } else {
-                        JOptionPane.showMessageDialog(v_principal, "El animal seleccionado no tiene visitas", "", JOptionPane.WARNING_MESSAGE);
+
+                if (v_principal.tablaAnimales.getSelectedRow() == -1) {
+
+                    JOptionPane.showMessageDialog(v_principal, "Por favor, selecciona un animal primero", "", JOptionPane.WARNING_MESSAGE);
+
+                } else {
+                    // Coger el id del animal
+                    int id = getSelectedAnimalID();
+
+                    try {
+                        if (principalDAO.hasVisits(id)) {
+                            V_VerVisita v_verVisita = new V_VerVisita();
+                            C_VerVisita c_verVisita = new C_VerVisita(v_verVisita, id);
+                            v_verVisita.setVisible(true);
+                        } else {
+                            JOptionPane.showMessageDialog(v_principal, "El animal seleccionado no tiene visitas", "", JOptionPane.WARNING_MESSAGE);
+                        }
+                    } catch (SQLException ex) {
+                        Logger.getLogger(C_Principal.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                } catch (SQLException ex) {
-                    Logger.getLogger(C_Principal.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                
+
             }
         });
-        
+
         // Poner la ventana al medio
         v_principal.setLocationRelativeTo(null);
-        
+
     }
-    
+
     /**
      * Actualiza la tabla
      */
-    
-    private void updateTable(){
-        
+    private void updateTable() {
+
         // Borra todas las filas de la tabla (por si acaso)
         DefaultTableModel model = (DefaultTableModel) v_principal.tablaAnimales.getModel();
         int nRows = model.getRowCount();
         for (int i = nRows - 1; i >= 0; i--) {
             model.removeRow(i);
         }
-                
-                
-        
+
         try {
             // Recoge la lista de productos de la BBDD
             ArrayList<M_Animal> animales = principalDAO.getAnimales();
@@ -220,10 +219,10 @@ public class C_Principal {
             // Añade las filas a la tabla
             for (M_Animal animal : animales) {
 
-                /***
+                /**
+                 * *
                  * Añadir las cosas en orden del arraylist al row
                  */
-
                 model.addRow(new Object[]{animal.getId(), animal.getNombre(), animal.getEspecie()});
             }
 
@@ -231,12 +230,12 @@ public class C_Principal {
             Logger.getLogger(C_Principal.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public int getSelectedAnimalID(){
+
+    public int getSelectedAnimalID() {
         int fila = v_principal.tablaAnimales.getSelectedRow();
         int id = (int) v_principal.tablaAnimales.getValueAt(fila, 0);
-        
+
         return id;
     }
-    
+
 }
