3b77b09d24b15126a9f6c1767d9f5879844bdb9a
noelia <noelia@noelia>
2019-05-21 17:23:39 +0200

    Creado DAO edit animal

---
+++ src/Pers/EditAnimalDAO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pers;
+
+import Modelo.M_Animal;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author noelia
+ */
+public class EditAnimalDAO  extends BaseDAO {
+
+    public EditAnimalDAO() {
+        connect();
+    }
+    
+
+    public void EditAnimal(int idAnimal) throws SQLException{
+        
+    }
+}
